ab6a1667ae4160ccdeaabc0eff4cce0afa9b3fe5
rabeb-bb <rabeb.bahry@gmail.com>
2020-01-26 17:41:20 +0100

    Update main.js
    comments to explain the iterations

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 console.log('Hello and welcome to My pyramid! choose the number or rows you want your pyramid to have!');
 function pyramid() {
   var numberOfRows= window.prompt('enter a number under 25');   //getting the user's input
-  var row="";
   if (numberOfRows>25){
     console.log('No can do!');
   }else{
       console.log('Your pyramid has '+numberOfRows+' rows:');
-      for (var i = 0; i <numberOfRows; i++) {
+      for (var i = 0; i <numberOfRows; i++) { //iteration for the each row
         var space = "";
-        for(let j = 0; j <(numberOfRows-i-1); j++) {
+        var row="";
+        for(let j = 0; j <(numberOfRows-i-1); j++) {  // iteration for the number of spaces needed to center the hashtags
         space += " ";
         }
-        for (var k = 1; k <= (2*i+1); k++) {
+        for (var k = 1; k <= (2*i+1); k++) { // iteration the number of hashtags needed in each row
           row=row+"#";
         }
 
