e00b4bfc759c2028964fd6d09113c3d9d1700248
rabeb-bb <rabeb.bahry@gmail.com>
2020-01-26 14:46:37 +0100

    Update main.js
    tweaks to improve the code: adding comments, reducing lines, giving more significant names

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 console.log('Hello and welcome to My pyramid! choose the number or rows you want your pyramid to have!');
 function pyramid() {
-    var n;
-    n= window.prompt('enter a number under 25');
-    var row="";
-    if (n>25){
-      console.log('No can do!');
-    }else{
-        console.log('Your pyramid has '+n+' rows:');
-    for (var i = 0; i <n; i++) {
-    var space = "";
-    for(let j = 0; j <(n-i-1); j++) {
+  var numberOfRows= window.prompt('enter a number under 25');   //getting the user's input
+  var row="";
+  if (numberOfRows>25){
+    console.log('No can do!');
+  }else{
+      console.log('Your pyramid has '+numberOfRows+' rows:');
+      for (var i = 0; i <numberOfRows; i++) {
+        var space = "";
+        for(let j = 0; j <(numberOfRows-i-1); j++) {
         space += " ";
-    }
+        }
         for (var k = 1; k <= (2*i+1); k++) {
-            row=row+"#";
+          row=row+"#";
         }
 
-        console.log(space+row);
-        row="";
+      console.log(space+row);
+      row=""; //clearing the current row to start a new one
     }
-}
+  }
 }
